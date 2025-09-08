d800bd9ce3f55fa10fe33e690f7f861cfa4bfde7
Michał Kiedrzyński <michkied@gmail.com>
2025-09-08 15:24:37 +0200

    Read param page

--- flash_programmer/flash_programmer.srcs/sources_1/imports/new/flash_programmer.vhd
+++ flash_programmer/flash_programmer.srcs/sources_1/imports/new/flash_programmer.vhd
@@ -153,7 +153,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity flash_programmer is
     Generic (
-        MAX_COUNT : integer := 25000000 * 5;
+        MAX_COUNT : integer := 25000000 / 1000;
         DELAY_MAX_COUNT : integer := 25000000
     );
     Port (
@@ -170,12 +170,12 @@ entity flash_programmer is
     nand_nre				: out std_logic := '1';
     nand_rnb				: in	std_logic;
     -- NAND chip data hardware interface. These signals should be boiund to physical pins.
-    nand_data			: inout	std_logic_vector(15 downto 0);
+    nand_data			: inout	std_logic_vector(15 downto 0)
     
     -- Debug
-        dbg_state   : out std_logic_vector(3 downto 0);
-        dbg_data_out : out	std_logic_vector(7 downto 0);
-        dbg_activate : out std_logic
+--        dbg_state   : out std_logic_vector(3 downto 0);
+--        dbg_data_out : out	std_logic_vector(7 downto 0);
+--        dbg_activate : out std_logic
     );
 end flash_programmer;
 
@@ -223,7 +223,7 @@ architecture Behavioral of flash_programmer is
 	signal activate : std_logic;
 	signal cmd_in   : std_logic_vector(7 downto 0);
 	
-	type STATE_TYPE is (INIT, RELEASE, CTRL_BUSY, RESET_DEV, ENABLE_DEV, RESET_CTRL, READ, EXTRACT, EXTRACT_READBYTE, DELAY);
+	type STATE_TYPE is (INIT, RELEASE, CTRL_BUSY, RESET_DEV, ENABLE_DEV, RESET_CTRL, READ_PARAM, READ_ID, EXTRACT, EXTRACT_READBYTE, DELAY);
 --    type STATE_TYPE is (INIT, RELEASE, CTRL_BUSY, RESET_DEV, ENABLE_DEV,  READ, EXTRACT, EXTRACT_READBYTE, DELAY);
 	signal state : STATE_TYPE := INIT;
 	signal next_state : STATE_TYPE;
@@ -262,32 +262,32 @@ begin
 		cmd_in   => cmd_in
 	);
 	
-	process(data_out)
-	begin
-	   dbg_data_out <= data_out;
-	end process;
+--	process(data_out)
+--	begin
+--	   dbg_data_out <= data_out;
+--	end process;
 	
-    process(activate)
-	begin
-	   dbg_activate <= activate;
-	end process;
+--    process(activate)
+--	begin
+--	   dbg_activate <= activate;
+--	end process;
 	
-	process(state)
-    begin
-        case state is
-            when INIT             => dbg_state <= "0000";
-            when RELEASE          => dbg_state <= "0001";
-            when CTRL_BUSY        => dbg_state <= "0010";
-            when RESET_DEV        => dbg_state <= "0011";
-            when ENABLE_DEV           => dbg_state <= "0100";
-            when RESET_CTRL => dbg_state <= "1001";
-            when READ             => dbg_state <= "0101";
-            when EXTRACT          => dbg_state <= "0110";
-            when EXTRACT_READBYTE => dbg_state <= "0111";
-            when DELAY            => dbg_state <= "1000";
-            when others           => dbg_state <= "1111";
-        end case;
-    end process;
+--	process(state)
+--    begin
+--        case state is
+--            when INIT             => dbg_state <= "0000";
+--            when RELEASE          => dbg_state <= "0001";
+--            when CTRL_BUSY        => dbg_state <= "0010";
+--            when RESET_DEV        => dbg_state <= "0011";
+--            when ENABLE_DEV           => dbg_state <= "0100";
+--            when RESET_CTRL => dbg_state <= "1001";
+--            when READ             => dbg_state <= "0101";
+--            when EXTRACT          => dbg_state <= "0110";
+--            when EXTRACT_READBYTE => dbg_state <= "0111";
+--            when DELAY            => dbg_state <= "1000";
+--            when others           => dbg_state <= "1111";
+--        end case;
+--    end process;
         
     process(CLK25MHZ)
     begin
@@ -343,10 +343,16 @@ begin
             
             when RESET_DEV =>
                 cmd_in <= x"01";
-                next_state <= READ;
+                next_state <= READ_PARAM;
                 state <= RELEASE;
+            
+            when READ_PARAM =>
+                data_in <= x"00";
+		        cmd_in <= x"02";
+		        next_state <= READ_ID;
+		        state <= RELEASE;
                 
-            when READ =>
+            when READ_ID =>
                 data_in <= x"00";
 		        cmd_in <= x"03";
 		        next_state <= RESET_CTRL;
@@ -358,8 +364,8 @@ begin
 		        state <= RELEASE;
                 
             when EXTRACT =>
---                cmd_in <= x"08";
-                cmd_in <= x"0e";
+                cmd_in <= x"08";
+--                cmd_in <= x"0e";
                 next_state <= EXTRACT_READBYTE;
                 state <= RELEASE;
                 
